703e3cb82707fd416ad8eda3f126730f009f32f4
YoussefKamal098 <kmalywsf098@gmail.com>
2023-03-20 17:49:50 -0700

    cmt

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - main
+ * description: Description
+ * Return: Return
+ */
+int main(void)
+{
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(1, quo, 59);
+return (1);
+}

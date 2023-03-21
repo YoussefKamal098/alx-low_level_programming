83b95054bda61204281a2850940fb6089ea180d8
YoussefKamal098 <kmalywsf098@gmail.com>
2023-03-20 17:17:08 -0700

    cmt

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,17 +9,13 @@
  */
 int main(void)
 {
-int i = 0;
-while (i < 10)
+int i = 48;
+while (i <= 102)
 {
 putchar(i + '0');
+if(i == 57)
+i+=39;
 i++;
-}
-char ch = 'a';
-while (ch <= 'f')
-{
-putchar(ch);
-ch++;
 }
 putchar('\n');
 return (0);

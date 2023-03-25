d89dc6c79749a037d111e53707c75c5263905088
YoussefKamal098 <kmalywsf098@gmail.com>
2023-03-24 19:56:44 -0700

    cmt

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - more_numbers
+ * Return: Return
+ */
+void more_numbers(void)
+{
+int num, row, count;
+for (row = 1; row <= 10; ++row)
+{
+for (count = 1; count <= 14; ++count)
+{
+num = count;
+if (num > 9)
+{
+_putchar(1 + 48);
+num = count % 10;
+}
+_putchar(num + 48);
+}
+}
+_putchar('\n');
+}

9c09e9029d0306c8cdbcdf11201b92ec5679b1c9
YoussefKamal098 <kmalywsf098@gmail.com>
2023-03-24 19:58:26 -0700

    cmt

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,7 +9,7 @@ void more_numbers(void)
 int num, row, count;
 for (row = 1; row <= 10; ++row)
 {
-for (count = 1; count <= 14; ++count)
+for (count = 0; count <= 14; ++count)
 {
 num = count;
 if (num > 9)

13b2db71118e0f5f1f2bf62abde38c571642ef37
YoussefKamal098 <kmalywsf098@gmail.com>
2023-03-27 15:47:33 -0700

    cmt

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swap_int
+ * description:decription
+ * @a:@a
+ * @b:@b
+ * return:return
+*/
+
+void swap_int(int *a, int *b)
+{int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}

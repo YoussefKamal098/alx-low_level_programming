3a195bebcd46d2bedbac21dc8c84aae31e56ec7b
YoussefKamal098 <kmalywsf098@gmail.com>
2023-04-03 16:06:57 -0700

    cmt

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - _strstr
+ * description:decription
+ * @haystack : @haystack
+ * @needle : @needle
+ * Return:return
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0;
+while (haystack[i] != '\0')
+{
+int j = 0;
+char *ptr = &haystack[i];
+while (haystack[i] == needle[j])
+{
+j++;
+if (needle[j] == '\0')
+{
+return (ptr);
+}
+}
+}
+return (NULL);
+}

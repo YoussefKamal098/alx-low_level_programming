1d27a4072b16d715d4fc535a04f77ea1ccd3cdb7
YoussefKamal098 <kmalywsf098@gmail.com>
2023-04-03 15:44:58 -0700

    cmt

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk -_strpbrk
+ * description:decription
+ * @s : @s
+ * @accept : @accept
+ * Return:return
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+char *p;
+i = 0;
+while (s[i] != '\0')
+{
+j = 0;
+while (accept[j] != '\0')
+{
+if (accept[j] == s[i])
+{
+p = &s[i];
+return (p);
+}
+j++;
+}
+i++;
+}
+return (0);
+}

c27584bfc145abb6622db102a2210fa095cacebe
YoussefKamal098 <kmalywsf098@gmail.com>
2023-04-05 15:08:30 -0700

    cmt

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,17 +3,31 @@
  * is_prime_number - is_prime_number
  * description:description
  * @n : @n
+ * @d : @d
  * Return:return
 */
 
+int check_prime(int n, int d);
+
 int is_prime_number(int n)
-{int i;
-if (n <= 1)
-return (0);
-for (i = 2; i * i <= n; i++)
 {
-if (n % i == 0)
-return (0);
+return (check_prime(n, 2));
 }
+
+/**
+ * check_prime - check_prime
+ * description:description
+ * @n : @n
+ * @d : @d
+ * Return:return
+*/
+
+int check_prime(int n, int d)
+{
+if (d >= n && n > 1)
 return (1);
+else if (n % d == 0 || n <= 1)
+return (0);
+else
+check_prime(n, d + 1);
 }

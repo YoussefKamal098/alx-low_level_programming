d2647d8e339171ab27724033bdf82dc70268c761
YoussefKamal098 <kmalywsf098@gmail.com>
2023-04-04 18:03:50 -0700

    cmt

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - set_string
+ * description:decription
+ * @s : @s
+ * @to : @to
+ * Return:return
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}

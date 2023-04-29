077e368a37a7ea400977d2daa5b518da37cdee4f
YoussefKamal098 <kmalywsf098@gmail.com>
2023-04-28 17:59:46 -0700

    cmt

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * _constructor-  _constructor
+ * Description:description
+ * Return:return
+ */
+
+void _constructor(void)
+{
+printf("You're beat! and yet, you must allow,\n
+I bore my house upon my back!\n");
+}

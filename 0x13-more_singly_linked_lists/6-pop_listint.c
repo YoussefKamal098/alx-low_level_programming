36414201d254a33e9ee0d538c60be5a006ff7890
YoussefKamal098 <kmalywsf098@gmail.com>
2023-04-30 13:01:06 -0700

    cmt

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,14 @@
 
 int pop_listint(listint_t **head)
 {
+listint_t *temp;
+int n;
+
 if (!head || !*head)
 return (0);
 
-listint_t *temp = *head;
-int n = temp->n;
+temp = *head;
+n = temp->n;
 *head = temp->next;
 
 free(temp);

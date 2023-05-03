6934cff0a64d16520b9dd4fdd69001438b636698
YoussefKamal098 <kmalywsf098@gmail.com>
2023-05-03 11:59:40 -0700

    cmt

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse_listint
+ * Description:description
+ * @head : head
+ * Return:return
+ */
+
+
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *curr, *pre;
+
+curr = *head;
+pre = NULL;
+
+while (curr)
+{
+curr = curr->next;
+(*head)->next = pre;
+pre = *head;
+*head = curr;
+}
+
+*head = pre;
+
+return (*head);
+}

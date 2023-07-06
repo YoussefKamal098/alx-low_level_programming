64b90199b40b6c33d5b1b70802fe5fcaabf4d9ec
YoussefKamal098 <kmalywsf098@gmail.com>
2023-07-06 15:43:19 -0700

    cmt

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - get_endianness
+ * Description:description
+ * Return:return
+ */
+
+int get_endianness(void)
+{
+unsigned long int n = 1;
+
+return (*(char *)&n);
+}

5a1b23b6f01faa4174f3dedd3991e3f8f811520d
YoussefKamal098 <kmalywsf098@gmail.com>
2023-07-11 10:36:58 -0700

    cmt

---
+++ 0x15-file_io/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h";
+
+/**
+ * _putchar - main
+ * @c:c
+ * description: Description
+ * Return: Return
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

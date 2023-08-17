fca41c626947ac519e4fe99126144b3f5ac66a64
YoussefKamal098 <kmalywsf098@gmail.com>
2023-08-17 03:29:50 -0700

    cmt

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ *sum_dlistint - sum_dlistint
+ * @head : head
+ * Description:description
+ * Return: sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+dlistint_t *temp = head;
+int sum = 0;
+
+if (!head)
+return (0);
+
+while (temp)
+{
+sum += temp->n;
+temp = temp->next;
+}
+
+return (sum);
+}

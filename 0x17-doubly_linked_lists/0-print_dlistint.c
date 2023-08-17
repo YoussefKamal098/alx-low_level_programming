a41dad5205cf23ffe54f5f42ce00ee7209c83065
YoussefKamal098 <kmalywsf098@gmail.com>
2023-08-17 02:46:40 -0700

    cmt

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print_dlistint
+ * @h : head
+ * Description:description
+ * Return: length of the Doubly-linkedList
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+const dlistint_t *temp = h;
+size_t size = 0;
+
+if (!h)
+return (0);
+
+while (temp)
+{
+printf("%i\n", temp->n);
+temp = temp->next;
+size++;
+}
+
+return (size);
+}

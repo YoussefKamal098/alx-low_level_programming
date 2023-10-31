6273ba4f253ec13cdcdf26ed6255f0cd461fd505
YoussefKamal098 <kmalywsf098@gmail.com>
2023-10-31 11:47:07 -0700

    cmt

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - (print_alphabet)
+ * Description : (Description)
+ * Return: (Return)
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+	for (ch = 97; ch < 123; ch++)
+		_putchar((char) ch);
+
+	_putchar('\n');
+}

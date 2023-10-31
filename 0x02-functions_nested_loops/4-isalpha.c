269c83254ee2c33b662b3b2dcad131e73f6f8142
YoussefKamal098 <kmalywsf098@gmail.com>
2023-10-31 15:22:05 -0700

    cmt

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,13 @@
 /**
  * isalpha - isalpha
  * @c : c
- * Return : return
+ * Return: return
  */
 int isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if (c >= 97 && c <= 122)
 		return (1);
-	else
-		return (0);
+	if (c >= 65 && c <= 90)
+		return (1)
+	return (0);
 }

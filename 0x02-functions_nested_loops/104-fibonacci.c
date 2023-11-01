1070fe80cabf1b44e89ab3a4371f8a955ab1bfc9
YoussefKamal098 <kmalywsf098@gmail.com>
2023-11-01 14:51:11 -0700

    cmt

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,37 +1,4 @@
 #include "main.h"
-/**
- * main - main
- * Return: return
- */
-int main(void)
-{
-	long i, t1 = 0, t2 = 1, sum;
-	float total_sum;
-
-	while (1)
-	{
-		sum = t1 + t2;
-
-		if (sum > 4000000)
-		{
-			break;
-		}
-
-		if (sum % 2 == 0)
-		{
-			total_sum += sum;
-		}
-
-		t1 = t2;
-		t2 = sum;
-	}
-
-	printf("%.0f\n", total_sum);
-
-	return (0);
-}
-
-#include "main.h"
 
 /**
  * num_length - num_length

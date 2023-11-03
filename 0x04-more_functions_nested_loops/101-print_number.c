2d2a5bc18a16d88b5bc36e3e532768ac6e5c1a99
YoussefKamal098 <kmalywsf098@gmail.com>
2023-11-03 05:44:21 -0700

    cmt

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _pow - _pow
+ * @x: x
+ * @y: y
+ * Return: return
+ */
+
+double _pow(double x, double y)
+{
+	int i;
+	double res = x;
+
+	for (i = 0; i < y - 1; i++)
+	{
+		res *= x;
+	}
+
+	return (res);
+}
+
+/**
+ * print_number - print_number
+ * @n: n
+ * Return: return
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	int digits_number, div;
+
+	digits_number = calc_of_digit(n);
+	div = (int)_pow(10, digits_number - 1);
+
+	while (div)
+	{
+		_putchar((n / div) + '0');
+		n = n % div;
+		div /= 10;
+	}
+}

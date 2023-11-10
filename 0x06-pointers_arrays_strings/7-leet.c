6af77b14402a9cb6a27b8e058c4593ea26e9d5f8
YoussefKamal098 <kmalywsf098@gmail.com>
2023-11-10 00:36:03 -0800

    cmt

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * to_lower - to_lower
+ * @c: c
+ * Return: return
+ */
+
+char to_lower(char c)
+{
+	if (c >= 97 || c <= 122)
+		return (c);
+
+	return (c + 32);
+}
+
+/**
  * leet - leet
  * @str: str
  * Return: return
@@ -11,18 +25,19 @@ char *leet(char *str)
 	int i, j;
 	const char decrypt[] = {'a', 'e', 'o', 't', 'l'};
 	const char encrypt[] = {'4', '3', '0', '7', '1'};
+	char *encrypted_str = str;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; encrypted_str[i] != '\0'; i++)
 	{
 		for (j = 0; j < sizeof(decrypt) / sizeof(decrypt[0]); j++)
 		{
-			if (str[i] == decrypt[j] || str[i] == decrypt[j] - 32)
+			if (to_lower(encrypted_str[i]) == decrypt[j])
 			{
-				str[i] = encrypt[j];
+				encrypted_str[i] = encrypt[j];
 				break;
 			}
 		}
 	}
 
-	return (str);
+	return (encrypted_str);
 }

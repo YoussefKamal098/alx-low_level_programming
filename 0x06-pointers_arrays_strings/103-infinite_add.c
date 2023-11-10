d775f8d58b3fc459c2b0a838b84123a280a05f96
YoussefKamal098 <kmalywsf098@gmail.com>
2023-11-10 02:38:45 -0800

    cmt

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,58 +11,43 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, tallest_number = 0, k, curr_n1_digit, curr_n2_digit, carry = 0;
+	int i = 0, j = 0, tallest = 0, k, curr_n1_d, curr_n2_d, c = 0;
 
 	while (n1[i])
 		i++;
 	while (n2[j])
 		j++;
-
 	if (i > j)
-		tallest_number = i;
+		tallest = i;
 	else
-		tallest_number = j;
-
-	if (tallest_number + 1 > size_r)
+		tallest = j;
+	if (tallest + 1 > size_r)
 		return (0);
-
-	r[tallest_number] = '\0';
-
-	for (k = tallest_number - 1; k >= 0; k--)
+	r[tallest] = '\0';
+	for (k = tallest - 1; k >= 0; k--)
 	{
 		i--;
 		j--;
-
 		if (i >= 0)
-			curr_n1_digit = n1[i] - '0';
+			curr_n1_d = n1[i] - '0';
 		else
-			curr_n1_digit = 0;
-
+			curr_n1_d = 0;
 		if (j >= 0)
-			curr_n2_digit = n2[j] - '0';
+			curr_n2_d = n2[j] - '0';
 		else
-			curr_n2_digit = 0;
-
-		r[k] = (curr_n1_digit + curr_n2_digit + carry) % 10 + '0';
-
-		carry = (curr_n1_digit + curr_n2_digit + carry) / 10;
+			curr_n2_d = 0;
+		r[k] = (curr_n1_d + curr_n2_d + c) % 10 + '0';
+		c = (curr_n1_d + curr_n2_d + c) / 10;
 	}
-
-	if (carry == 1)
+	if (c == 1)
 	{
-		r[tallest_number + 1] = '\0';
-
-		if (tallest_number + 2 > size_r)
+		r[tallest + 1] = '\0';
+		if (tallest + 2 > size_r)
 			return (0);
-
-		while (tallest_number-- >= 0)
-		{
-			printf("%d\n", tallest_number);
-			r[tallest_number + 1] = r[tallest_number];
-		}
-
-		r[0] = carry + '0';
+		while (tallest-- > 0)
+			r[tallest + 1] = r[tallest];
+		r[0] = c + '0';
 	}
-
 	return (r);
 }
+

05f9e46eb720a2e48dfb033451de02c8fae6ad35
YoussefKamal098 <kmalywsf098@gmail.com>
2023-11-21 06:56:45 -0800

    cmt

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,6 +15,9 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
+	if (grid == NULL)
+		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
 		int *row = (int *)malloc(sizeof(int) * width);

7ca743e59785ee898daca6ebccfacb277ab3b7ab
YoussefKamal098 <kmalywsf098@gmail.com>
2023-11-28 01:51:35 -0800

    cmt

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * print_dog - print_dog
+ * @d: d
+ * Return: return
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,87 @@
+#include "dog.h"
+
+/**
+ * _strlen - _strlen
+ * @str: str
+ * Return: return
+ */
+
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	if (str == NULL)
+		return (0);
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * _strdup - _strdup
+ * @str: str
+ * Return: return
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *dup_str;
+
+	if (str == NULL)
+		return (NULL);
+
+	dup_str = (char *)malloc(_strlen(str) + 1);
+
+	if (dup_str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		dup_str[i] = str[i];
+
+	dup_str[i] = '\0';
+
+	return (dup_str);
+}
+
+/**
+ * new_dog - new_dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: return
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog = (dog_t *)malloc(sizeof(dog_t));
+	char *name_cpy, *owner_cpy;
+
+	if (dog == NULL)
+		return (NULL);
+
+	name_cpy = _strdup(name);
+
+	if (name_cpy == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = name_cpy;
+
+	owner_cpy = _strdup(owner);
+
+	if (owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = owner_cpy;
+	dog->age = age;
+
+	return (dog);
+}

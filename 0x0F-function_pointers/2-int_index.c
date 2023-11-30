72054c68e62e5136f9c1bbf6f2376550bb4d3578
YoussefKamal098 <kmalywsf098@gmail.com>
2023-11-30 11:54:10 -0800

    cmt

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,23 +2,23 @@
 
 /**
  * int_index - int_index
- * description:description
  * @array : @array
  * @size : size
  * @cmp : cmp
  * Return:return
  */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i = 0;
-if (array && size, cmp)
-while (i < size)
-{
-if (cmp(array[i])
-return (i);
-i++;
-}
+	int i;
 
-return (-1);
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
 }

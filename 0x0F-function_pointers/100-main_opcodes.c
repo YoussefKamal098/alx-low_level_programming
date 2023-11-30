2b0f6e5193682f0f81b2850e73c7c6c79ba5e824
YoussefKamal098 <kmalywsf098@gmail.com>
2023-11-30 14:21:03 -0800

    cmt

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,27 @@
+#include "function_pointer.h"
+
+/**
+ * main - main
+ * @ac: ac
+ * @av: av
+ * Return: return
+ */
+
+int main(int ac, char *av[])
+{
+	char *p = (char *)main;
+	int b;
+
+	if (ac != 2)
+		printf("Error\n"), exit(1);
+
+	b = atoi(av[1]);
+
+	if (b < 0)
+		printf("Error\n"), exit(2);
+
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+
+	return (0);
+}

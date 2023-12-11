4af8af1d73c1132e15aa0768f1942a833d361f4d
YoussefKamal098 <kmalywsf098@gmail.com>
2023-12-11 11:53:26 -0800

    cmt

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,35 +1,5 @@
 #include "lists.h"
 /**
- * add_node_to_list - add_node_to_list
- * @list: list
- * @size: size
- * @node: node
- * Return: return
- */
-listint_t **add_node_to_list(listint_t **list, size_t size, listint_t *node)
-{
-	listint_t **new_list;
-	size_t i;
-
-	new_list = (listint_t **)malloc(sizeof(listint_t *) * (size + 1));
-
-	if (new_list == NULL)
-	{
-		if (list)
-			free(list);
-		exit(98);
-	}
-
-	for (i = 0; i < size; i++)
-		new_list[i] = list[i];
-
-	new_list[i] = node;
-
-	free(list);
-	return (new_list);
-}
-
-/**
  * find_loop - find_loop
  * @head:head
  * Return: return
@@ -38,34 +8,28 @@ listint_t **add_node_to_list(listint_t **list, size_t size, listint_t *node)
 listint_t *find_loop(listint_t *head)
 {
 
-	listint_t **list = NULL;
-	size_t size = 0, i;
+	listint_t *curr, *ptr;
 
 	if (!head)
 		return (NULL);
 
-	while (head && head->next)
+	curr = head;
+	while (curr)
 	{
-		for (i = 0; i < size; i++)
+		if (curr == curr->next)
+			return (curr);
+
+		ptr = head;
+		while (ptr != curr)
 		{
-			if (head->next == list[i])
-			{
-				free(list);
-				return (head);
-			}
+			if (curr->next == ptr)
+				return (curr);
+			ptr = ptr->next;
 		}
 
-		list = add_node_to_list(list, size, head);
-
-		if (list == NULL)
-			return (NULL);
-
-		head = head->next;
-		size++;
+		curr = curr->next;
 	}
 
-	if (list)
-		free(list);
 	return (NULL);
 }
 

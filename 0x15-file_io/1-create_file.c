8847329f927ee7d04ccf88b4e0b6226035370a49
YoussefKamal098 <kmalywsf098@gmail.com>
2023-12-18 05:07:51 -0800

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -39,7 +39,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	text_len = strlen(text_content);
+	text_len = _strlen(text_content);
 	if (text_len)
 		bytes = write(fd, text_content, text_len);
 

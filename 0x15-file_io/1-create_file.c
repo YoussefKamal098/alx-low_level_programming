8576e6dc6c4fba0dbbfb7c749af4fc160dcaa653
YoussefKamal098 <kmalywsf098@gmail.com>
2023-12-18 05:04:22 -0800

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,49 @@
 #include "main.h"
 
 /**
+ * _strlen - length of string
+ * @str: string
+ * Return: length of string
+ */
+
+size_t _strlen(char *str)
+{
+	size_t i = 0;
+
+	if (str == NULL || *str == '\0')
+		return (0);
+
+	while (str[i])
+		i++;
+
+	return (i);
+}
+
+/**
  * create_file - create_file
- * @filename : @filename
- * @text_content : @text_content
- * Description:description
+ * @filename:  filename
+ * @text_content: text_content
  * Return:return
  */
 
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-ssize_t bytes = 0, text_len = strlen(text_content);
+	int fd;
+	ssize_t bytes = 0, text_len;
 
-if (!filename)
-{
-return (-1);
-}
+	if (!filename)
+		return (-1);
 
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-if (fd == -1)
-{
-return (-1);
-}
+	if (fd == -1)
+		return (-1);
 
-if (text_len)
-{
-bytes = write(fd, text_content, text_len);
-}
+	text_len = strlen(text_content);
+	if (text_len)
+		bytes = write(fd, text_content, text_len);
 
-close(fd);
+	close(fd);
 
-return (bytes == text_len ? 1 : -1);
+	return (bytes == text_len ? 1 : -1);
 }

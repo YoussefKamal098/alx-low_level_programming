2c23783afbae7fb611caf75d1595982a9af0f821
YoussefKamal098 <kmalywsf098@gmail.com>
2024-01-29 19:24:52 +0200

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://github.com/YoussefKamal098/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
+wget -P /tmp https://github.com/YoussefKamal098/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
